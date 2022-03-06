22d904abcc369ecd289b1167398e4def48af32d7
nelodev <nelodev@users.noreply.github.com>
2022-03-06 14:08:46 +0000

    deploy: e48f3220103a51459a397a13394e9513fe261853

---
+++ _dist_/lazy.js
@@ -0,0 +1,21 @@
+const isIntersecting = (entry) => {
+    return entry.isIntersecting;
+};
+
+const loadImage = (entry) => {
+  const container = entry.target;
+  const image = container.firstChild;
+  const url = image.dataset.src;
+  image.src = url;
+  observer.unobserve(container);
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries
+        .filter(isIntersecting)
+        .forEach(loadImage)
+});
+
+export const registerImage = (image) => {
+    observer.observe(image);
+};
